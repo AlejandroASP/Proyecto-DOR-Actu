f2529de6d5de27412409d0b41ac6bc0c4cfe5595
AlejandroASP <alejandroasp71@gmail.com>
2023-12-21 09:43:57 +0000

    Añadiendo página para probar index

---
+++ js/burguer.js
@@ -0,0 +1,24 @@
+const navigation = document.getElementById("nav");
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", () => {
+  // The navigation.children.length means the following :-
+  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
+  //   Yup That's all.
+  navigation.style.setProperty("--childenNumber", navigation.children.length);
+
+  //    Casually Toggling Classes to make them animate on click
+  //   Regular stuff ;)
+  navigation.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+var counter = 2;
+
+setInterval(function(){
+  document.getElementById('btn' + counter).click();
+  counter++;
+  // console.log(counter);
+  if (counter == 4){
+    counter = 1;
+  }
+}, 5000);
